adb89466fa8c92b982a2c70e63882e06fa34b2c5
Mateo Del Lungo <mateodellungo@gmail.com>
2024-11-08 23:15:38 -0300

    Update FakeContext.cs

--- Proyecto/Backend/FarmaciaWebAPI/DataAccess/Models/FakeContext.cs
+++ Proyecto/Backend/FarmaciaWebAPI/DataAccess/Models/FakeContext.cs
@@ -25,13 +25,13 @@ namespace DataAccess.Models
             
             Users =
             [
-                //hola mundo
+                // contraseña hola mundo
                 new User { Id = 1, Username = "mudo", Password = "0b894166d3336435c800bea36ff21b29eaa801a52f584c006c49289a0dcf6e2f" },
-                //111
+                // contraseña 111
                 new User { Id = 2, Username = "mateo", Password = "f6e0a1e2ac41945a9aa7ff8a8aaa0cebc12a3bcc981a929ad5cf810a090e11ae" },
-                //1234
+                // contraseña 1234
                 new User { Id = 3, Username = "juan", Password = "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4" },
-                //test
+                // contraseña test
                 new User { Id = 4, Username = "test", Password = "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08" },
             ];
 
